67635681e6854ddd2885af4d10063dae27e36c5e
Ana Valdemoro Ramirez <Ana@ADEdelivered.com>
2024-02-09 17:17:10 +0000

    feat: create controllers for main routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { helloWorld } from "../controllers/photo.controller";
 
 const router = Router();
+
+router.route("/").get(helloWorld);
 
 export default router;
