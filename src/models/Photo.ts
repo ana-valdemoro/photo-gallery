2ccb224e3641fa254e755dd465d5fa82f0233fc8
Ana Valdemoro Ramirez <Ana@ADEdelivered.com>
2024-02-09 16:52:37 +0000

    feat: define our photo schema

---
+++ src/models/Photo.ts
@@ -0,0 +1,16 @@
+import { Schema, model, Document } from "mongoose";
+
+const schema = new Schema({
+  title: String,
+  description: String,
+  imagePath: String,
+});
+
+interface IPhoto extends Document {
+  title: string;
+  description: string;
+  imagePath: string;
+}
+
+// Obeys the mode to match the structure
+export default model<IPhoto>("Photo", schema);
