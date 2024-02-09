c84c1755b7ac53821d8739762a35cbc8644fe1eb
Ana Valdemoro Ramirez <Ana@ADEdelivered.com>
2024-02-09 12:16:42 +0000

    feat: installed morgan to track incoming request on server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 
 // settings
 app.set("port", process.env.port || 4000);
+
+// middleware
+app.use(morgan("dev"));
 
 export default app;
